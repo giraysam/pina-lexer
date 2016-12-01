f4b08c81f86ccb673e23d5892d6023e39e7beba6
giray sam <gry.sam@gmail.com>
2016-12-01 18:46:04 +0300

    fix: key name in the test file.

--- test/pina-lexer.js
+++ test/pina-lexer.js
@@ -25,7 +25,7 @@ describe("Tests for 'var x = 2;' ", function(){
     });
 
     it("case 2: first token key should be 'IDENTIFIER'", function () {
-        tokens[0].key.should.equal('IDENTIFIER');
+        tokens[0].tokenType.should.equal('IDENTIFIER');
     });
 
     it("case 3: second token value should be 'x'", function () {
@@ -33,15 +33,15 @@ describe("Tests for 'var x = 2;' ", function(){
     });
 
     it("case 4: third token key should be 'EQUALS' and value '='", function () {
-        tokens[2].key.should.equal('EQUALS') && tokens[2].value.should.equal('=');
+        tokens[2].tokenType.should.equal('EQUALS') && tokens[2].value.should.equal('=');
     });
 
     it("case 5: fourth token key should be 'NUMBER'", function () {
-        tokens[3].key.should.equal('NUMBER');
+        tokens[3].tokenType.should.equal('NUMBER');
     });
 
     it("case 6: last token key should be 'SEMI'", function () {
-        tokens[4].key.should.equal('SEMI');
+        tokens[4].tokenType.should.equal('SEMI');
     });
 
 });
